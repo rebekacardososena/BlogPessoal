502605990229e77aab55d3144c64114ece1345d6
Rebeka Sena <bekacristine@gmail.com>
2022-08-04 14:12:27 -0300

    Atualização

---
+++ blogpessoalbeka/src/main/java/org/generation/blogpessoalbeka/BlogpessoalbekaApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogpessoalbeka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoalbekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoalbekaApplication.class, args);
+	}
+
+}
---
+++ blogpessoalbeka/src/main/java/org/generation/blogpessoalbeka/repository/TemaRepository.java
@@ -0,0 +1,11 @@
+package org.generation.blogpessoalbeka.repository;
+
+import java.util.List;
+
+import org.generation.blogpessoalbeka.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
